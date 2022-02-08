0e62c94858eb31fb2141795eeb157e50ca5ebfcf
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-02-07 19:19:08 -0600

    add new Event link with placeholder page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,21 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
+  resources :events, only: [:new, :show, :create, :index]
 
   root 'events#index'
 end
