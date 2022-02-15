d4c779cb7916fd1c0669b9bb31d4960f0ec432d4
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-02-14 19:45:38 -0600

    add future/past scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
 
   has_many :event_attendance, foreign_key: :attended_event
   has_many :attendees, through: :event_attendance, source: :attendee
+
+  scope :future, -> { where('date >= ?', Date.today) }
+  scope :past, -> { where('date < ?', Date.today) }
 end
