3da805c989c00c6940a5359fae433583e4ef0eb7
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-02-14 20:22:02 -0600

    add private column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_012208) do
+ActiveRecord::Schema.define(version: 2022_02_15_015335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_012208) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "creator_id", null: false
+    t.boolean "private", default: false, null: false
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
