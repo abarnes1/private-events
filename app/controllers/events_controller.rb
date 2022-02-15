367e3bc21f26d519740349b557d75b745e8d7e1d
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-02-14 20:22:47 -0600

    add public/private to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,6 +50,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :location, :date)
+    params.require(:event).permit(:name, :location, :date, :private)
   end
 end
