c5abaaccdaff771505757d69c9f36f39be36b96d
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-02-15 21:18:42 -0600

    add/remove event attendance working

--- app/controllers/event_attendance_controller.rb
+++ app/controllers/event_attendance_controller.rb
@@ -2,23 +2,35 @@ class EventAttendanceController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def create
-    @event_attendance = current_user.event_attendance.build(attended_event_id: params[:attended_event_id])
+    @event_attendance = current_user.event_attendance.build(
+      attended_event_id: params[:attended_event_id],
+      invitation_status: params[:invitation_status].to_i
+    )
+
     @event_attendance.save
 
     redirect_to event_path(@event_attendance.attended_event_id)
   end
 
   def destroy
-    # need to figure this out by primary key
-    @event_attendance = current_user.event_attendance.where(attended_event_id: params[:attended_event_id]).first
-    EventAttendance.destroy(@event_attendance.id)
+    @event_attendance = if params[:attended_event_id]
+                          EventAttendance.where(
+                            attended_event_id: params[:attended_event_id],
+                            attendee_id: params[:attendee_id]
+                          ).first
+                        else
+                          EventAttendance.find(params[:id])
+                        end
 
-    redirect_to event_path(params[:attended_event_id])
+    event_id = @event_attendance.attended_event_id
+    @event_attendance.destroy
+
+    redirect_to event_path(event_id)
   end
 
   private
 
   def event_attendance_params
-    require(:event_attendance).permit(:attended_event_id, :attendee_id)
+    require(:event_attendance).permit(:attended_event_id, :attendee_id, :invitation_status)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
   has_many :event_attendance, foreign_key: :attendee
   has_many :attended_events, through: :event_attendance, source: :attended_event
+
+  def attending?(event)
+    attended_events.include?(event)
+  end
 end
