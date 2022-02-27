9bd9e33e2d09578b28d63c9d28720488ba9f26c6
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-02-26 18:18:45 -0600

    add Event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User'
+  validate :event_date_cannot_be_in_past
+  validates :date, presence: true
+  validates :name, length: { in: 5..100 }, presence: true
+  validates :location, length: { in: 5..100 }, presence: true
 
+  belongs_to :creator, class_name: 'User'
   has_many :event_attendance
 
   has_many :invitations, -> { where(attendance_status: :invited) },
@@ -11,6 +15,10 @@ class Event < ApplicationRecord
 
   scope :future, -> { where('date >= ?', Date.today) }
   scope :past, -> { where('date < ?', Date.today) }
+
+  def event_date_cannot_be_in_past
+    errors.add(:date, "can't be in the past") if date < Date.today
+  end
 
   def upcoming?
     date >= Date.today
