b7f612c2e38c33ab7ea59912a422dcde24eece09
Aaron Barnes <aaron.barnes1@hotmail.com>
2022-03-29 23:00:58 -0500

    Prevent viewing another user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    # prevent accessing another user's page because the create event button is not protected
+    redirect_to user_path(current_user.id) if params[:id] != current_user.id.to_s
+
+    @user = User.find(current_user.id)
   end
 end
